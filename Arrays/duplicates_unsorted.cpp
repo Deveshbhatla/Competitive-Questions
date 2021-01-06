7cb4e0cf82f65c3453d56b4f4199de71296bf96a
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-01-06 19:20:27 +0530

    Update duplicates_unsorted.cpp

--- Arrays/duplicates_unsorted.cpp
+++ Arrays/duplicates_unsorted.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
 
+    for(int i=0;i<n-1;i++)
+    {
+        int count =1;
+        if(arr[i]!=-1)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                    arr[j]=-1;
+
+                }    
+            }
+        }
+        if(count>1)
+        {       
+            cout<<arr[i]<<" "<<count;
+        }
+    }
+
+     for (int i=0;i<n;i++)
+    {
+        cout<<" "<<arr[i];
+    }
+    return 0;
+}
