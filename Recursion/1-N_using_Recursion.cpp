581678d598905bda198ca50a3627a4c12de23905
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-01-17 21:43:55 +0530

    Update 1-N_using_Recursion.cpp

--- Recursion/1-N_using_Recursion.cpp
+++ Recursion/1-N_using_Recursion.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n)
+{
+    if(n==0)
+     return;
+    //cout<<n<<" "; If cout here instead of below it will print n to 1
+    print(n-1);
+        cout<<n<<" ";
 
+}
+int main()
+{
+    print(3);
+    return 0;
+}
