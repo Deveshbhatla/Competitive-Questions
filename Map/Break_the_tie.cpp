6018c295f402f1ed7adfe4972d04896ada51eaad
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-01-20 23:39:54 +0530

    Update Break_the_tie.cpp

--- Map/Break_the_tie.cpp
+++ Map/Break_the_tie.cpp
@@ -1,1 +1,42 @@
+//Given an array of names of candidates in an election. A candidate name in array represents a vote casted to the candidate. 
+//You need to find the candidate with maximum votes recieved in the election.
+//It may happen that two or more candidates may have the same votes, in that case you need to return the name of the candidate which comes first lexicographically in dictionary.
+#include <bits/stdc++.h>
+using namespace std;
 
+string TieBreak(string names[], int n)
+{
+     map<string,int> map;
+    for(int i=0;i<n;i++)
+    {
+        map[names[i]]++;
+    }
+    int max=0;
+    string winner;
+    for(auto itr=map.begin();itr!=map.end();itr++)
+    {
+        if(itr->second>max)
+        {
+            max=itr->second;
+            winner=itr->first;
+        }
+    }
+        return winner;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        string names[n];
+        for(int i = 0; i < n; i++)
+            cin>>names[i];
+        cout << TieBreak(names,n) <<endl; 
+    }
+}
