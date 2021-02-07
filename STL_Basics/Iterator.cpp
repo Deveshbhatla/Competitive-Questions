1aa7dd98600794796d11c20a033294dbc115871f
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-02-07 20:16:59 +0530

    Update Iterator.cpp

--- STL_Basics/Iterator.cpp
+++ STL_Basics/Iterator.cpp
@@ -1,39 +1,31 @@
+
 #include <bits/stdc++.h>
+//#include <vector>
 using namespace std;
 
-template <typename T>
-
-// A structure can also be used instead of a class as it has public access type
-struct Pair 
-{
-    // These pairs can be of any type
-    T x, y;
-
-    // Constructor accpecting both parameters of same type
-    Pair(T i, T j)
-    {
-        x = i;
-        y = j;
-    }
-
-    // Function of T type
-    T getFirst()
-    {
-        return x;
-    }
-
-    // Function of T type
-    T getSecond()
-    {
-        return y;
-    }
-};
-
+// Driver Method
 int main()
 {
-    // Pair needed to be passed
-    Pair<int> p1(1, 2);
-    cout << p1.getFirst() << " " << p1.getSecond();
+    vector<int> v = { 10, 20, 30, 40, 50 };
+
+    // i points to the first element 10
+    vector<int>::iterator i = v.begin();//or auto i=v.begin();
+    
+
+    // By default next() moves one position ahead
+    i = next(i);
+
+    // Prints the value 20
+    cout << (*i) << " ";//20
+
+    // Moves i two positions ahead
+     i = next(i, 2);
+
+    cout << (*i) << " ";//40
+
+    // Moves back one position Pointing to 30
+    i = prev(i);
+
+    cout << (*i) << " ";// 30
     return 0;
 }
-
