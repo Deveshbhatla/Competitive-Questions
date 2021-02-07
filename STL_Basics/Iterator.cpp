5599b613d8162e1a0a3d465bf83a08a10302f58b
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-02-07 20:16:49 +0530

    Update Iterator.cpp

--- STL_Basics/Iterator.cpp
+++ STL_Basics/Iterator.cpp
@@ -1,1 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+
+// A structure can also be used instead of a class as it has public access type
+struct Pair 
+{
+    // These pairs can be of any type
+    T x, y;
+
+    // Constructor accpecting both parameters of same type
+    Pair(T i, T j)
+    {
+        x = i;
+        y = j;
+    }
+
+    // Function of T type
+    T getFirst()
+    {
+        return x;
+    }
+
+    // Function of T type
+    T getSecond()
+    {
+        return y;
+    }
+};
+
+int main()
+{
+    // Pair needed to be passed
+    Pair<int> p1(1, 2);
+    cout << p1.getFirst() << " " << p1.getSecond();
+    return 0;
+}
 
