c06aaaa1f60cee0331a2b9c760e7c9c24cd0361d
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-02-12 21:38:09 +0530

    Update vector.cpp

--- Vector/vector.cpp
+++ Vector/vector.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -33,4 +34,56 @@ int main()
     for(auto itr2 = v.begin(); itr2!=v.end();itr2++)
         cout<<*itr2<<" ";
     return 0;
+}
+
+//Some basic programs for vectors
+int findFrequency(vector<int> v, int x){
+    
+    int freq = count(v.begin(), v.end(), x);
+    return freq;
+    
+}
+vector<int> sortVector(vector<int>v)
+{
+   sort(v.begin(),v.end());
+    return v;
+}
+vector<int> reverseVector(vector<int>v)
+{
+    reverse(v.begin(), v.end()); 
+    return v;
+}
+void eraseAt(vector<int> &V, int pos)
+{
+    vector<int> :: iterator itr=V.begin();
+    itr+=pos;
+    V.erase(itr);
+ 
+    
+}
+
+void eraseInRange(vector<int> &V, int start, int end)
+{
+    vector<int> :: iterator itr1=V.begin();
+     vector<int> :: iterator itr2=V.begin();
+    itr1+=start;
+        itr2+=end;
+
+    V.erase(itr1,itr2);
+}
+
+void clearAll(vector<int> &V)
+{
+   V.clear();
+    
+}
+vector<int> list_less_than_k(vector<int> a,int n,int k)
+{
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<k)
+        v.push_back(a[i]);
+    }
+    return v;
 }
