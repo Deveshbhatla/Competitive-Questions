8ccdffdf548b8bb28f5f76134f90dc25ce812b68
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-03-06 23:23:55 +0530

    Update Find_Number_of_digits.cpp

--- Mathematics/Find_Number_of_digits.cpp
+++ Mathematics/Find_Number_of_digits.cpp
@@ -1,1 +1,26 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int countDigits(int x)//O(result)
+{
+	int result = 0;
+	while(x > 0)
+	{
+			x = x / 10;
+			result++;
+	}	
+	return result;
+}
+int countDigitsEfficient(int N) //O(1)
+{ 
+    int k;
+    k = floor(log10(N) + 1);
+    return k;
+}
+int main() {
+    
+    	int number = 789;
+    	
+    	cout<<countDigits(number);
+    	cout<<countDigitsEfficient(number);
+    	return 0;
+}
