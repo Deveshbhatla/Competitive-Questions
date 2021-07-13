679e5a652c993065b9af454ee8ed441d8ef48f09
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-07-13 23:23:20 +0530

    Updated ReverseWords.cpp

--- String/ReverseWords.cpp
+++ String/ReverseWords.cpp
@@ -2,38 +2,41 @@
 // output:there Hello
 #include<bits/stdc++.h>
 using namespace std;
-void reverse(char str[],int low, int high){
-    while(low<=high){
-        swap(str[low],str[high]);
-        low++;
-        high--;
+ void reverse(string &str,int low, int high){
+    int n = high - low;
+
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i + low], str[n - i - 1 + low]);
     }
 }
 
-void ReverseWords(char str[], int n)
-{
-    int start =0;
-    for(int end=0;end<n;end++)
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        int start =0;
+    for(int end=0;end<S.length();end++)
     {
-        if(str[end]==' ')
+        if(S[end]==' ')
         {
-            reverse(str,start,end-1);
+            reverse(S,start,end);
             start=end+1;
 
         }
     }
-     reverse(str,start,n-1);
-    reverse(str,0,n-1);
-}
+reverse(S,start,S.length());
+reverse(S,0,S.length());
+return S;
+
+    } 
 int main() 
 { 
     string s = "Hello there";
     int n=s.length();
     char str[n];
+
     strcpy(str, s.c_str());
-    ReverseWords(str,n);
-    for (int i = 0; i < n; i++) 
-        cout << str[i];
+
+    cout<<reverseWords(s);
     
     return 0; 
 } 
