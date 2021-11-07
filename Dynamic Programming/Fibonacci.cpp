111b627463bbb9a6701d1f7b4e71c3c82acd5e01
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-11-07 23:02:51 +0530

    "Dynamic Programming/Fibonacci.cpp":

--- Dynamic Programming/Fibonacci.cpp
+++ Dynamic Programming/Fibonacci.cpp
@@ -1,11 +1,10 @@
-//Memoization
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int memo[1000000];
 
-int fibonacciRecursion(int n)
+int fibonacciRecursion(int n)//O(2^n)
 {
     if (n == 0 || n == 1)
         return n;
@@ -13,7 +12,8 @@ int fibonacciRecursion(int n)
         return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
 }
 
-int fibonacciDynamic(int n)
+//Memoization(Top down)
+int fibonacciMemoization(int n)//O(n)
 {
     if (memo[n] == -1)
     {
@@ -21,17 +21,30 @@ int fibonacciDynamic(int n)
         if (n == 0 || n == 1)
             result = n;
         else
-            result = fibonacciDynamic(n - 1) + fibonacciDynamic(n - 2);
+            result = fibonacciMemoization(n - 1) + fibonacciMemoization(n - 2);
 
         memo[n] = result;
     }
     return memo[n];
 }
 
+//Tabulation(Bottom up)
+int fibonacciTabulation(int n)
+{
+    int f[n+1];
+    f[0]=0, f[1]=1;
+
+    for (int i = 2; i <=n; i++)
+        f[i]=f[i-1]+f[i-2];
+
+    return f[n];    
+    
+}
+ 
 int main()
 {
     memset(memo, -1, sizeof(memo));
 
     int n = 5;
-    cout << fibonacciDynamic(8);
+    cout << fibonacciTabulation(8);
 }
