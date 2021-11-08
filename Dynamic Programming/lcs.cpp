f1c62a63dda636f59b0705465f24f0fb0e7ba7e0
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2021-11-08 23:24:40 +0530

    "Dynamic Programming/lcs.cpp":

--- Dynamic Programming/lcs.cpp
+++ Dynamic Programming/lcs.cpp
@@ -34,6 +34,31 @@ int lcsMemoization(string s1, string s2, int m, int n)//O(mn)
     
 }
 
+//Tabulation
+int lcsTabulation(string s1, string s2, int m, int n)
+{
+    int dp[m+1][n+1];
+
+    //assigning the first row and columns as 0
+    for(int i=0;i<m;i++)
+        dp[i][0]=0;
+
+    for(int j=0;j<n;j++)
+        dp[0][j]=0;
+
+    for(int i=1; i<=m; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(s1[i-1]==s2[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    
+    return dp[m][n];    
+}
 int main()
 {
 
@@ -41,6 +66,6 @@ int main()
     
 	string s1="AXYZ", s2="BAZ";
     int m=4,n=3;
-    cout<<lcsMemoization(s1,s2,m,n);
+    cout<<lcsTabulation(s1,s2,m,n);
     return 0;
 }
