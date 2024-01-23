9c5a2e7e17b7f4e97b9124f7e53a66001ad916a0
Devesh Bhatla <deveshbhatla952@gmail.com>
2024-01-22 23:27:43 -0800

    Array

---
+++ 150 LeetCode/Array and Strings/122M-Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -0,0 +1,44 @@
+//You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+//On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
+//However, you can buy it then immediately sell it on the same day.
+
+//Find and return the maximum profit you can achieve.
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+// Uses DP with bottom up approach with time complexity of O(n) and space complexity of O(1)
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &prices)
+{
+
+    //as we cannot sell on the first day, hence, setting the initial value to a large negative number;
+    int currentHolding = INT_MIN;
+    int currentNotHolding = 0;
+
+    for (int i = 0; i < prices.size(); i++)
+    {
+        //initializing two variables that keep track of the previous holdings
+        int previousHolding = currentHolding;
+        int previousNotHolding = currentNotHolding;
+
+        //either you can hold the previous holding or buy a new stock today
+        currentHolding = max(previousHolding, previousNotHolding - prices[i]);
+
+        //either you can hold on to the previous stock that you were not holding, or sell stock today if you found a better stock
+        currentNotHolding = max(previousNotHolding, previousHolding + prices[i]);
+
+    }
+    return currentNotHolding;
+}
+
+int main()
+{
+    vector<int> v={7,1,5,3,6,4};//output should be 7
+    cout<<"max profit: "<<maxProfit(v);
+    
+    return 0;
+}
---
+++ 150 LeetCode/Array and Strings/55M-JumpGame.cpp
@@ -0,0 +1,50 @@
+// You are given an integer array nums. You are initially positioned at the array's first index,
+// and each element in the array represents your maximum jump length at that position.
+
+// Return true if you can reach the last index, or false otherwise.
+
+// https://leetcode.com/problems/jump-game/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+//Greedy solution 
+//Time Complexity:O(n), space complexity: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canJump(vector<int> &nums)
+{
+    int maximumReach = 0;
+    int n = nums.size();
+
+    // if the first element itself is greater than the last one, then we can return true without going into the loop
+    if (nums[0] >= n)
+        return true;
+
+    for (int i = 0; i < n; i++)
+    {
+        // current jump distance as the array element
+        int currentJumpDistance = nums[i];
+
+        // if the maximum reach is less than i, then return false, as we cannot jump till the end of the array
+        if (maximumReach < i)
+            return false;
+
+        // calculate the maximum reach by taking the max of itself and the current i and the jump distance
+        maximumReach = max(maximumReach, i + currentJumpDistance);
+
+        // again, if the maximum reach is bigger than n, no need to continue the loop again
+        if (maximumReach >= n)
+            return true;
+    }
+    return true;
+}
+
+int main()
+{
+    vector<int> nums = {3, 2, 1, 0, 4}; // false
+    string result = canJump(nums) ? "true" : "false";
+
+    cout << result;
+    return 0;
+}
